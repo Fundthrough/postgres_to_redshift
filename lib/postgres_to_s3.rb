bbde0a9567fb7b50cd10aa466fac2a793ed6a009
kahim <tchui@fundthrough.com>
2017-05-04 12:38:18 -0400

    first s3 commit

--- lib/postgres_to_s3.rb
+++ lib/postgres_to_s3.rb
@@ -7,47 +7,35 @@ require 'tempfile'
 require "postgres_to_redshift/table"
 require "postgres_to_redshift/column"
 
-class PostgresToRedshift
+class PostgresToS3
   class << self
-    attr_accessor :source_uri, :target_uri, :target_schema, :source_schema, :delete_option
+    attr_accessor :source_uri, :source_schema, :source_table
   end
 
-  attr_reader :source_connection, :target_connection, :s3
+  attr_reader :source_connection, :s3
 
   KILOBYTE = 1024
   MEGABYTE = KILOBYTE * 1024
   GIGABYTE = MEGABYTE * 1024
 
   def self.update_tables
-    update_tables = PostgresToRedshift.new
+    update_tables = PostgresToS3.new
 
     update_tables.tables.each do |table|
-      #target_connection.exec("CREATE TABLE IF NOT EXISTS #{target_schema}.#{target_connection.quote_ident(table.target_table_name)} (#{table.columns_for_create})")
-
       update_tables.copy_table(table)
-
-      update_tables.import_table(table)
     end
   end
 
   def self.source_uri
-    @source_uri ||= URI.parse(ENV['P2RS_SOURCE_URI'])
+    @source_uri ||= URI.parse(ENV['P2S3_SOURCE_URI'])
   end
 
   def self.source_schema
-    @source_schema ||= ENV['P2RS_SOURCE_SCHEMA']
+    @source_schema ||= ENV['P2S3_SOURCE_SCHEMA']
   end
 
-  def self.target_schema
-    @target_schema ||= ENV['P2RS_TARGET_SCHEMA']
-  end
-
-  def self.target_uri
-    @target_uri ||= URI.parse(ENV['P2RS_TARGET_URI'])
-  end
-
-  def self.delete_option
-    @delete_option ||= ENV["P2RS_DELETE_OPTION"]
+  def self.source_table
+    @source_schema ||= ENV['P2S3_SOURCE_TABLE']
   end
 
   def self.source_connection
@@ -59,24 +47,12 @@ class PostgresToRedshift
     @source_connection
   end
 
-  def self.target_connection
-    unless instance_variable_defined?(:"@target_connection")
-      @target_connection = PG::Connection.new(host: target_uri.host, port: target_uri.port, user: target_uri.user || ENV['USER'], password: target_uri.password, dbname: target_uri.path[1..-1])
-    end
-
-    @target_connection
-  end
-
   def source_connection
     self.class.source_connection
   end
 
-  def target_connection
-    self.class.target_connection
-  end
-
   def tables
-    source_connection.exec("SELECT * FROM information_schema.tables WHERE table_schema = '#{PostgresToRedshift.source_schema}' AND table_type in ('BASE TABLE') AND table_name NOT IN ('ar_internal_metadata','schema_migrations') AND LEFT(table_name,1) != '_'").map do |table_attributes|
+    source_connection.exec("SELECT * FROM information_schema.tables WHERE table_schema = '#{PostgresToS3.source_schema}' AND table_name = '#{PostgresToS3.source_table}'").map do |table_attributes|
       table = Table.new(attributes: table_attributes)
       next if table.name =~ /^pg_/
       table.columns = column_definitions(table)
@@ -85,15 +61,15 @@ class PostgresToRedshift
   end
 
   def column_definitions(table)
-    source_connection.exec("SELECT * FROM information_schema.columns WHERE table_schema = '#{PostgresToRedshift.source_schema}' AND table_name='#{table.name}' order by ordinal_position")
+    source_connection.exec("SELECT * FROM information_schema.columns WHERE table_schema = '#{PostgresToS3.source_schema}' AND table_name='#{table.name}' order by ordinal_position")
   end
 
   def s3
-    @s3 ||= AWS::S3.new(access_key_id: ENV['P2RS_S3_EXPORT_ID'], secret_access_key: ENV['P2RS_S3_EXPORT_KEY'])
+    @s3 ||= AWS::S3.new(access_key_id: ENV['P2S3_S3_EXPORT_ID'], secret_access_key: ENV['P2S3_S3_EXPORT_KEY'])
   end
 
   def bucket
-    @bucket ||= s3.buckets[ENV['P2RS_S3_EXPORT_BUCKET']]
+    @bucket ||= s3.buckets[ENV['P2S3_S3_EXPORT_BUCKET']]
   end
 
   def copy_table(table)
@@ -102,11 +78,11 @@ class PostgresToRedshift
     chunksize = 5 * GIGABYTE # uncompressed
     chunk = 1
 
-    bucket.objects.with_prefix("#{PostgresToRedshift.target_schema}/#{table.target_table_name}.psv.gz").delete_all
+    #bucket.objects.with_prefix("#{PostgresToS3.target_schema}/#{table.target_table_name}.psv.gz").delete_all
 
     begin
       puts "DOWNLOADING #{table}"
-      copy_command = "COPY (SELECT #{table.columns_for_copy} FROM #{PostgresToRedshift.source_schema}.#{table.name}) TO STDOUT WITH DELIMITER '|'"
+      copy_command = "COPY (SELECT #{table.columns_for_copy} FROM #{PostgresToS3.source_schema}.#{table.name}) TO STDOUT WITH DELIMITER '|'"
 
       source_connection.copy_data(copy_command) do
         while row = source_connection.get_copy_data
@@ -136,38 +112,7 @@ class PostgresToRedshift
   def upload_table(table, buffer, chunk)
     puts "UPLOADING #{table.target_table_name}.#{chunk}"
 
-    bucket.objects["#{PostgresToRedshift.target_schema}/#{table.target_table_name}.psv.gz.#{chunk}"].write(buffer, acl: :authenticated_read)
+    bucket.objects["#{PostgresToS3.source_schema}/#{table.target_table_name}.psv.gz.#{chunk}"].write(buffer, acl: :authenticated_read)
 
-  end
-
-  def import_table(table)
-
-    puts "IMPORTING #{PostgresToRedshift.target_schema}.#{table.target_table_name}"
-
-    if PostgresToRedshift.delete_option == 'drop'
-
-      puts "DROP TABLE IF EXISTS #{PostgresToRedshift.target_schema}.#{table.target_table_name}"
-      target_connection.exec("DROP TABLE IF EXISTS #{PostgresToRedshift.target_schema}.#{table.target_table_name}")
-
-      puts "CREATE TABLE #{PostgresToRedshift.target_schema}.#{table.target_table_name}"
-      target_connection.exec("CREATE TABLE #{PostgresToRedshift.target_schema}.#{target_connection.quote_ident(table.target_table_name)} (#{table.columns_for_create})")
-
-      puts "COPY TABLE to #{PostgresToRedshift.target_schema}.#{table.target_table_name}"
-      target_connection.exec("COPY #{PostgresToRedshift.target_schema}.#{target_connection.quote_ident(table.target_table_name)} FROM 's3://#{ENV['P2RS_S3_EXPORT_BUCKET']}/#{PostgresToRedshift.target_schema}/#{table.target_table_name}.psv.gz' CREDENTIALS 'aws_access_key_id=#{ENV['P2RS_S3_EXPORT_ID']};aws_secret_access_key=#{ENV['P2RS_S3_EXPORT_KEY']}' GZIP TRUNCATECOLUMNS ESCAPE DELIMITER as '|' COMPUPDATE ON;")
-
-    elsif PostgresToRedshift.delete_option == 'truncate'
-
-      puts "CREATE TABLE IF NOT EXISTS #{PostgresToRedshift.target_schema}.#{table.target_table_name}"
-      target_connection.exec("CREATE TABLE IF NOT EXISTS #{PostgresToRedshift.target_schema}.#{target_connection.quote_ident(table.target_table_name)} (#{table.columns_for_create})")
-
-      puts "TRUNCATE TABLE #{PostgresToRedshift.target_schema}.#{table.target_table_name}"
-      target_connection.exec("TRUNCATE TABLE #{PostgresToRedshift.target_schema}.#{table.target_table_name}")
-
-      puts "COPY TABLE to #{PostgresToRedshift.target_schema}.#{table.target_table_name}"
-      target_connection.exec("COPY #{PostgresToRedshift.target_schema}.#{target_connection.quote_ident(table.target_table_name)} FROM 's3://#{ENV['P2RS_S3_EXPORT_BUCKET']}/#{PostgresToRedshift.target_schema}/#{table.target_table_name}.psv.gz' CREDENTIALS 'aws_access_key_id=#{ENV['P2RS_S3_EXPORT_ID']};aws_secret_access_key=#{ENV['P2RS_S3_EXPORT_KEY']}' GZIP TRUNCATECOLUMNS ESCAPE DELIMITER as '|' COMPUPDATE ON;")
-
-    else
-      puts "missing delete_option"
-    end
   end
 end
