ddba81c47b4dd7d9ac6cdb3844a6f7a925c76226
kahim <tchui@fundthrough.com>
2017-05-05 12:00:18 -0400

    voert to csv with | as quote

--- lib/postgres_to_s3.rb
+++ lib/postgres_to_s3.rb
@@ -88,7 +88,7 @@ class PostgresToS3
 
     begin
       puts "DOWNLOADING #{table}"
-      copy_command = "COPY (SELECT #{table.columns_for_copy} FROM #{PostgresToS3.source_schema}.#{table.name} WHERE lower(service_name) = lower('#{PostgresToS3.service_name}') AND created_at::date = '#{PostgresToS3.archive_date}') TO STDOUT WITH DELIMITER '|'"
+      copy_command = "COPY (SELECT #{table.columns_for_copy} FROM #{PostgresToS3.source_schema}.#{table.name} WHERE lower(service_name) = lower('#{PostgresToS3.service_name}') AND created_at::date = '#{PostgresToS3.archive_date}') TO STDOUT WITH DELIMITER ',' QUOTE'|'"
 
       source_connection.copy_data(copy_command) do
         while row = source_connection.get_copy_data
